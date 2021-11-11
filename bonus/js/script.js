b891f22a155d32263a8cee6bc19fdf699596ad9b
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2021-11-12 00:03:07 +0100

    Bonus Ex

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -15,34 +15,37 @@ var carousel = new Vue ({
                 image: 'img/01.jpg',
                 title: 'Svezia',
                 text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-                contatore :0
+                 
             },
             {
                 image: 'img/02.jpg',
                 title: 'Svizzera',
                 text: 'Lorem ipsum',
-                contatore: 1
+            
             },
             {
                 image: 'img/03.jpg',
                 title: 'Gran Bretagna',
                 text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-                contatore: 2
+             
             },
             {
                 image: 'img/04.jpg',
                 title: 'Germania',
                 text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-                contatore: 3
+              
             },
             {
                 image: 'img/05.jpg',
                 title: 'Paradise',
                 text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-                contatore: 4
+            
             }
 
-        ] 
+        ] ,
+        // playerActive : "",
+        activePlay : "player_none",
+        activePause : "player_none",
     },
     methods : {
         nextSlide:function(){
@@ -61,9 +64,41 @@ var carousel = new Vue ({
             }
             
         },
+        play:function(){
+            this.go = setInterval(() => {
+                if ( this.activeSlide >= this.slides.length - 1){
+                    this.activeSlide = 0
+                } else {
+                    this.activeSlide ++;
+                }
+            }, 3000);
+            if ( this.activePlay === "player_none") {
+                this.activePlay = "player_active"
+                this.activePause = "player_none"
+            } else {
+                this.activePlay = "player_none";
+            }
+            
+        },
+        stop:function(){
+            clearInterval(this.go);
+            if ( this.activePause === "player_none") {
+                this.activePause = "player_active"
+                this.activePlay = "player_none"
+            } else {
+                this.activePause = "player_none";
+            }
+        }
        
-    }
-
+    },
+    // mounted() {
+    //     var stop = setInterval(() => {
+    //         if ( this.activeSlide >= this.slides.length - 1){
+    //             this.activeSlide = 0
+    //         } else {
+    //             this.activeSlide ++;
+    //         }
+    //     }, 3000);
+    //   },
+      
 });
-
-
